473010a190004afb19b5dc8ef6b9b9578f2865c9
vdhoan <156833044+vdhoan@users.noreply.github.com>
2024-12-08 18:23:14 +0700

    Update mm.h

--- OS-ASS/include/mm.h
+++ OS-ASS/include/mm.h
@@ -94,9 +94,9 @@
 
 /* Memory range operator */
 /* TODO implement the INCLUDE checking mechanism - currently dummy op only */
-#define INCLUDE(x1,x2,y1,y2) (0)
+#define INCLUDE(x1,x2,y1,y2) (((y1-x1)*(x2-y2)>=0)?1:0)
 /* TODO implement the OVERLAP checking mechanism - currently dummy op only */
-#define OVERLAP(x1,x2,y1,y2) (1)
+#define OVERLAP(x1,x2,y1,y2) (((y2-x1)*(x2-y1)>=0)?1:0)
 
 /* VM region prototypes */
 struct vm_rg_struct * init_vm_rg(int rg_start, int rg_endi, int vmaid);
