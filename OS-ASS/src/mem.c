99fa89c585452855d6d4ed319c5fcdef50888e63
vdhoan <156833044+vdhoan@users.noreply.github.com>
2024-12-08 19:51:30 +0700

    Update mem.c

--- OS-ASS/src/mem.c
+++ OS-ASS/src/mem.c
@@ -8,12 +8,12 @@
 static BYTE _ram[RAM_SIZE];
 
 static struct {
-	uint32_t proc; // ID of process currently uses this page
-	int index;	   // Index of the page in the list of pages allocated
-	// to the process.
-	int next; // The next page in the list. -1 if it is the last
-	// page.
-} _mem_stat[NUM_PAGES];
+	uint32_t proc;	// ID of process currently uses this page
+	int index;	// Index of the page in the list of pages allocated
+			// to the process.
+	int next;	// The next page in the list. -1 if it is the last
+			// page.
+} _mem_stat [NUM_PAGES];
 
 static pthread_mutex_t mem_lock;
 
@@ -24,7 +24,9 @@ void init_mem(void) {
 }
 
 /* get offset of the virtual address */
-static addr_t get_offset(addr_t addr) { return addr & ~((~0U) << OFFSET_LEN); }
+static addr_t get_offset(addr_t addr) {
+	return addr & ~((~0U) << OFFSET_LEN);
+}
 
 /* get the first layer index */
 static addr_t get_first_lv(addr_t addr) {
@@ -37,10 +39,10 @@ static addr_t get_second_lv(addr_t addr) {
 }
 
 /* Search for page table table from the a segment table */
-static struct trans_table_t *
-get_trans_table(addr_t index,					   // Segment level index
-				struct page_table_t *page_table) { // first level table
-
+static struct trans_table_t * get_trans_table(
+		addr_t index, 	// Segment level index
+		struct page_table_t * page_table) { // first level table
+	
 	/* DO NOTHING HERE. This mem is obsoleted */
 
 	int i;
@@ -48,26 +50,27 @@ get_trans_table(addr_t index,					   // Segment level index
 		// Enter your code here
 	}
 	return NULL;
+
 }
 
 /* Translate virtual address to physical address. If [virtual_addr] is valid,
  * return 1 and write its physical counterpart to [physical_addr].
  * Otherwise, return 0 */
-static int translate(addr_t virtual_addr,	// Given virtual address
-					 addr_t *physical_addr, // Physical address to be returned
-					 struct pcb_t *proc) { // Process uses given virtual address
+static int translate(
+		addr_t virtual_addr, 	// Given virtual address
+		addr_t * physical_addr, // Physical address to be returned
+		struct pcb_t * proc) {  // Process uses given virtual address
 
 	/* Offset of the virtual address */
 	addr_t offset = get_offset(virtual_addr);
-	offset++;
-	offset--;
+        offset++; offset--;
 	/* The first layer index */
 	addr_t first_lv = get_first_lv(virtual_addr);
 	/* The second layer index */
 	addr_t second_lv = get_second_lv(virtual_addr);
-
+	
 	/* Search in the first level */
-	struct trans_table_t *trans_table = NULL;
+	struct trans_table_t * trans_table = NULL;
 	trans_table = get_trans_table(first_lv, proc->page_table);
 	if (trans_table == NULL) {
 		return 0;
@@ -80,29 +83,27 @@ static int translate(addr_t virtual_addr,	// Given virtual address
 			return 1;
 		}
 	}
-	return 0;
+	return 0;	
 }
 
-addr_t alloc_mem(uint32_t size, struct pcb_t *proc) {
+addr_t alloc_mem(uint32_t size, struct pcb_t * proc) {
 	pthread_mutex_lock(&mem_lock);
 	addr_t ret_mem = 0;
 	/* DO NOTHING HERE. This mem is obsoleted */
 
-	uint32_t num_pages =
-		(size % PAGE_SIZE)
-			? size / PAGE_SIZE
-			: size / PAGE_SIZE + 1; // Number of pages we will use
+	uint32_t num_pages = (size % PAGE_SIZE) ? size / PAGE_SIZE :
+		size / PAGE_SIZE + 1; // Number of pages we will use
 	int mem_avail = 0; // We could allocate new memory region or not?
 
 	/* First we must check if the amount of free memory in
 	 * virtual address space and physical address space is
-	 * large enough to represent the amount of required
+	 * large enough to represent the amount of required 
 	 * memory. If so, set 1 to [mem_avail].
 	 * Hint: check [proc] bit in each page of _mem_stat
 	 * to know whether this page has been used by a process.
 	 * For virtual memory space, check bp (break pointer).
 	 * */
-
+	
 	if (mem_avail) {
 		/* We could allocate new memory region to the process */
 		ret_mem = proc->bp;
@@ -118,27 +119,27 @@ addr_t alloc_mem(uint32_t size, struct pcb_t *proc) {
 	return ret_mem;
 }
 
-int free_mem(addr_t address, struct pcb_t *proc) {
+int free_mem(addr_t address, struct pcb_t * proc) {
 	/* DO NOTHING HERE. This mem is obsoleted */
 	return 0;
 }
 
-int read_mem(addr_t address, struct pcb_t *proc, BYTE *data) {
+int read_mem(addr_t address, struct pcb_t * proc, BYTE * data) {
 	addr_t physical_addr;
 	if (translate(address, &physical_addr, proc)) {
 		*data = _ram[physical_addr];
 		return 0;
-	} else {
+	}else{
 		return 1;
 	}
 }
 
-int write_mem(addr_t address, struct pcb_t *proc, BYTE data) {
+int write_mem(addr_t address, struct pcb_t * proc, BYTE data) {
 	addr_t physical_addr;
 	if (translate(address, &physical_addr, proc)) {
 		_ram[physical_addr] = data;
 		return 0;
-	} else {
+	}else{
 		return 1;
 	}
 }
@@ -149,15 +150,24 @@ void dump(void) {
 		if (_mem_stat[i].proc != 0) {
 			printf("%03d: ", i);
 			printf("%05x-%05x - PID: %02d (idx %03d, nxt: %03d)\n",
-				   i << OFFSET_LEN, ((i + 1) << OFFSET_LEN) - 1,
-				   _mem_stat[i].proc, _mem_stat[i].index, _mem_stat[i].next);
+				i << OFFSET_LEN,
+				((i + 1) << OFFSET_LEN) - 1,
+				_mem_stat[i].proc,
+				_mem_stat[i].index,
+				_mem_stat[i].next
+			);
 			int j;
-			for (j = i << OFFSET_LEN; j < ((i + 1) << OFFSET_LEN) - 1; j++) {
-
+			for (	j = i << OFFSET_LEN;
+				j < ((i+1) << OFFSET_LEN) - 1;
+				j++) {
+				
 				if (_ram[j] != 0) {
 					printf("\t%05x: %02x\n", j, _ram[j]);
 				}
+					
 			}
 		}
 	}
 }
+
+
