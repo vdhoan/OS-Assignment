3d3fc5b12db4a66bfa955cb5a59eae335ace7e63
vdhoan <156833044+vdhoan@users.noreply.github.com>
2024-12-08 19:52:14 +0700

    Update cpu.c

--- OS-ASS/src/cpu.c
+++ OS-ASS/src/cpu.c
@@ -3,50 +3,54 @@
 #include "mem.h"
 #include "mm.h"
 
-int calc(struct pcb_t *proc) { return ((unsigned long)proc & 0UL); }
+int calc(struct pcb_t * proc) {
+	return ((unsigned long)proc & 0UL);
+}
 
-int alloc(struct pcb_t *proc, uint32_t size, uint32_t reg_index) {
+int alloc(struct pcb_t * proc, uint32_t size, uint32_t reg_index) {
 	addr_t addr = alloc_mem(size, proc);
 	if (addr == 0) {
 		return 1;
-	} else {
+	}else{
 		proc->regs[reg_index] = addr;
 		return 0;
 	}
 }
 
-int free_data(struct pcb_t *proc, uint32_t reg_index) {
+int free_data(struct pcb_t * proc, uint32_t reg_index) {
 	return free_mem(proc->regs[reg_index], proc);
 }
 
-int read(struct pcb_t *proc,	 // Process executing the instruction
-		 uint32_t source,		 // Index of source register
-		 uint32_t offset,		 // Source address = [source] + [offset]
-		 uint32_t destination) { // Index of destination register
-
+int read(
+		struct pcb_t * proc, // Process executing the instruction
+		uint32_t source, // Index of source register
+		uint32_t offset, // Source address = [source] + [offset]
+		uint32_t destination) { // Index of destination register
+	
 	BYTE data;
-	if (read_mem(proc->regs[source] + offset, proc, &data)) {
+	if (read_mem(proc->regs[source] + offset, proc,	&data)) {
 		proc->regs[destination] = data;
-		return 0;
-	} else {
+		return 0;		
+	}else{
 		return 1;
 	}
 }
 
-int write(struct pcb_t *proc,	// Process executing the instruction
-		  BYTE data,			// Data to be wrttien into memory
-		  uint32_t destination, // Index of destination register
-		  uint32_t offset) {	// Destination address =
-	// [destination] + [offset]
+int write(
+		struct pcb_t * proc, // Process executing the instruction
+		BYTE data, // Data to be wrttien into memory
+		uint32_t destination, // Index of destination register
+		uint32_t offset) { 	// Destination address =
+					// [destination] + [offset]
 	return write_mem(proc->regs[destination] + offset, proc, data);
-}
+} 
 
-int run(struct pcb_t *proc) {
+int run(struct pcb_t * proc) {
 	/* Check if Program Counter point to the proper instruction */
 	if (proc->pc >= proc->code->size) {
 		return 1;
 	}
-
+	
 	struct inst_t ins = proc->code->text[proc->pc];
 	proc->pc++;
 	int stat = 1;
@@ -62,6 +66,11 @@ int run(struct pcb_t *proc) {
 		stat = alloc(proc, ins.arg_0, ins.arg_1);
 #endif
 		break;
+#ifdef MM_PAGING
+	case MALLOC:
+		stat = pgmalloc(proc, ins.arg_0, ins.arg_1);
+		break;
+#endif
 	case FREE:
 #ifdef MM_PAGING
 		stat = pgfree_data(proc, ins.arg_0);
@@ -87,4 +96,7 @@ int run(struct pcb_t *proc) {
 		stat = 1;
 	}
 	return stat;
+
 }
+
+
